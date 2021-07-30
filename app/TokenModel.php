fc2701c03f797f52f14b2d23615775500a821152
syahroel712 <syahroel712@gmail.com>
2021-07-30 12:09:58 +0700

    CRUD Token

--- app/TokenModel.php
+++ app/TokenModel.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class TokenModel extends Model
 {
-    //
+    protected $table = "tb_token";
+    protected $primaryKey = 'token_id';
+    protected $fillable = [
+        'token_tanggal',
+        'token_jam',
+        'token_key',
+    ];
+    public $timestamps = false;
 }
---
+++ resources/views/backend/pages/token/index.blade.php
@@ -0,0 +1,125 @@
+@extends('backend/layouts/app')
+@section('content')
+            <!-- Content Header (Page header) -->
+            <section class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1>Token Ujian</h1>
+                        </div>
+                        <div class="col-sm-6">
+                            <ol class="breadcrumb float-sm-right">
+                                <li class="breadcrumb-item"><a href="#">SWIMOC CAT</a></li>
+                                <li class="breadcrumb-item active">Token Ujian</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div><!-- /.container-fluid -->
+            </section>
+
+            <!-- Main content -->
+            <section class="content">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-12">
+                            <!-- Default box -->
+                            <div class="card">
+                                <div class="card-header">
+                                    <h3 class="card-title"><a class="btn btn-primary btn-sm text-white" href="{{ route('token.create') }}"><i class="fa fa-plus"></i> Add</a></h3>
+                                    <div class="card-tools">
+                                        <button type="button" class="btn btn-tool" data-card-widget="collapse"
+                                            data-toggle="tooltip" title="Collapse">
+                                            <i class="fas fa-minus"></i></button>
+                                        <button type="button" class="btn btn-tool" data-card-widget="remove"
+                                            data-toggle="tooltip" title="Remove">
+                                            <i class="fas fa-times"></i></button>
+                                    </div>
+                                </div>
+                                <div class="card-body">
+                                
+                                    <div class="callout callout-success" style="display:none" id="message">
+                                        <strong>{{ session()->get('message') }}</strong>
+                                        <button type="button" class="close" data-dismiss="alert"><span>&times;</span></button>
+                                    </div>
+
+                                    <table id="example1" class="table table-bordered table-striped example1">
+                                        <thead>
+                                            <tr>
+                                                <th>No</th>
+                                                <th>Tanggal</th>
+                                                <th>Jam</th>
+                                                <th>Token</th>
+                                                <th>Option</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            @foreach($token as $no => $mpl)
+                                            <tr>
+                                                <td>{{ $no+1 }}</td>
+                                                <td>{{ \Carbon\Carbon::parse($mpl->token_tanggal)->isoFormat('D MMMM Y') }}</td>
+                                                <td>{{ $mpl->token_jam }}</td>
+                                                <td>{{ $mpl->token_key }}</td>
+                                                <td>
+                                                    <a href="{{ route('token.edit', $mpl->token_id) }}" class="btn btn-warning btn-sm"><i class="fa fa-edit"></i></a>
+                                                    <button type="button" class="btn btn-danger btn-sm" onclick="mHapus('{{ route('token.delete', $mpl->token_id) }}')"><i class="fa fa-trash"></i> </button>
+                                                </td>
+                                            </tr>
+                                            @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <!-- /.card-body -->
+                                <div class="card-footer">
+                                    
+                                </div>
+                                <!-- /.card-footer-->
+                            </div>
+                            <!-- /.card -->
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- /.content -->
+
+            <!-- modal hapus -->
+            <div class="modal fade" id="ModalHapus" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+                aria-hidden="true" data-backdrop="static" data-keyboard="false">
+                <div class="modal-dialog modal-lg" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title">Hapus Data</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <form action="" method="POST" id="formDelete">
+                            <div class="modal-body">
+                                @csrf
+                                @method('delete')
+                                Yakin Hapus Data ?
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary btn-sm" data-dismiss="modal">Close</button>
+                                <button type="submit" class="btn btn-info btn-sm">Delete</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+            <script>
+                // untuk hapus data
+                function mHapus(url) {
+                    $('#ModalHapus').modal()
+                    $('#formDelete').attr('action', url);
+                }
+            </script>
+
+            @if (session()->has('message'))
+            <script>
+                $('#message').show();
+                setInterval(function(){ $('#message').hide(); }, 5000);
+            </script>
+            @endif
+
+@endsection
